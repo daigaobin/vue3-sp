2a93ddecc4e3f6d1e9a9016ff70ef753d21d9159
daigb <daigb@youzu.com>
2022-04-07 16:21:38 +0800

    feat(第一次提交): 使用cz提交代码

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 牧鱼
  * @Date: 2022-04-07 16:00:16
- * @LastEditTime: 2022-04-07 16:00:16
+ * @LastEditTime: 2022-04-07 16:13:28
  * @LastEditors: 牧鱼
  * @Description:
  * @FilePath: \vue3-sp\commitlint.config.js
@@ -9,23 +9,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-leading-blank": [2, "always"], // body换行
-    "header-max-length": [2, "never", 72], // header 最长72
+    // "body-leading-blank": [2, "always"], // body换行
+    // "header-max-length": [2, "never", 72], // header 最长72
     "type-enum": [
       2,
       "always",
       [
-        "add", // 创建功能
-        "del", // 删除功能
-        "fix", // 解决问题
-        "bump", // 修改某个版本号
-        "conf", // 配置文件修改
-        "refactor", // 必须进行重构的代码
-        "reformat", // 代码格式化
-        "optimize", // 代码性能优化
-        "doc", // 文档构建与修改
-        "start", // 开始做某事，比如创建分支等
-        "end", // 结束做某事，比如删除分支等
+        "feat", //新功能 feature
+        "fix", //修复Bug
+        "docs", //文档变更
+        "style", //代码格式
+        "refactor", //重构
+        "perf", //性能优化
+        "test", //增加测试
+        "chore", //构建过程或者辅助工具变动
+        "revert", //回退
+        "build", //打包
       ],
     ],
   },
